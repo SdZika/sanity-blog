8afd66378fa9fb65037be31f65019de6bcea1ce6
SdZika <markozivkovic1982@gmail.com>
2024-09-20 15:12:21 +0200

    more change on schema

--- schemaTypes/post.ts
+++ schemaTypes/post.ts
@@ -1,30 +1,27 @@
 import {defineField, defineType} from 'sanity'
 
 export const post = defineType({
-    title: 'Post',
-    name: 'post',
-    type: 'document',
-    fields: [defineField(
-      {
-        name: 'name',
-        type: 'string',
-        
-      }),
-      defineField({
-        name: 'slug',
-        type: 'slug',
-        options: {source: 'name'}
-      }),
-      defineField({
-        name: 'publishedAt',
-        type: 'datetime',
-      }),
-      defineField({
-        name: 'body',
-        type: 'array',
-        of: [{type: 'block',}],
-      }),
-    ],
-    
-  })
-  
+  title: 'Post',
+  name: 'post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'slug',
+      type: 'slug',
+      options: {source: 'name'},
+    }),
+    defineField({
+      name: 'publishedAt',
+      type: 'date',
+    }),
+    defineField({
+      name: 'body',
+      type: 'array',
+      of: [{type: 'block'}],
+    }),
+  ],
+})
