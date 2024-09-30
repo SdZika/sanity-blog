8bc93c57fced5b1fc9427515aeec9c32f68f1bed
SdZika <markozivkovic1982@gmail.com>
2024-09-30 15:17:13 +0200

    link env file

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -5,9 +5,6 @@ export default defineCliConfig({
     projectId: 'wqxigzwn',
     dataset: 'production'
   },
-  /**
-   * Enable auto-updates for studios.
-   * Learn more at https://www.sanity.io/docs/cli#auto-updates
-   */
-  autoUpdates: true,
+  studioHost: 'zikasd',
+  autoUpdates: false,
 })
